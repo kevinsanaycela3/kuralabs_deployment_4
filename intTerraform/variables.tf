46bb4343d8e7bc80b4707f4acfcb93a449393cc0
Kevin Sanaycela <kevinsanaycela@Kevins-MacBook-Air.local>
2022-11-01 23:06:02 -0400

    bruh

--- intTerraform/variables.tf
+++ intTerraform/variables.tf
@@ -3,11 +3,11 @@
 variable "aws_access_key" {
   description = "access key"
   type        = string
-  sensitive   = true
+  #sensitive   = true
 }
 
 variable "aws_secret_key" {
   description = "secret key"
   type        = string
-  sensitive   = true
+  #sensitive   = true
 }
